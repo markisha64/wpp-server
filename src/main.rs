fecb920342da01fc1e06e87bc30b29576a0c31d4
markisha64 <marko.grizelj03@gmail.com>
2025-01-28 16:34:38 +0100

    feat: initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/test")]
+async fn test() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(test))
+        .bind("127.0.0.1:3030")?
+        .run()
+        .await
 }
