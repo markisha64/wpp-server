c4bb1a980b40c9036e87c817c450768910b309d6
markisha64 <marko.grizelj03@gmail.com>
2025-02-02 10:56:17 +0100

    feat: join

--- src/api/chat.rs
+++ src/api/chat.rs
@@ -1,7 +1,7 @@
 use actix_web::{error, web, Responder};
 use anyhow::Context;
-use mongodb::bson::doc;
-use serde::Deserialize;
+use mongodb::bson::{doc, oid::ObjectId};
+use serde::{Deserialize, Serialize};
 
 use crate::{models::chat::Chat, mongodb::MongoDatabase};
 
@@ -41,6 +41,49 @@ async fn create(
     Ok(web::Json(chat))
 }
 
+#[derive(Serialize)]
+struct JoinResponse {}
+
+async fn join(
+    db: web::Data<MongoDatabase>,
+    user: web::ReqData<Claims>,
+    id: web::Path<String>,
+) -> actix_web::Result<impl Responder> {
+    let collection = db.database.collection::<Chat>("chats");
+
+    let chat_id =
+        ObjectId::parse_str(id.into_inner()).map_err(|err| error::ErrorBadRequest(err))?;
+
+    let chat = collection
+        .find_one(doc! {
+            "_id": &chat_id
+        })
+        .await
+        .map_err(|err| error::ErrorInternalServerError(err))?
+        .context("chat not found")
+        .map_err(|err| error::ErrorNotFound(err))?;
+
+    if chat.user_ids.contains(&user.user.id) {
+        return Ok(web::Json(JoinResponse {}));
+    }
+
+    db.database
+        .collection::<Chat>("chats")
+        .update_one(
+            doc! {
+                "_id": &chat_id
+            },
+            doc! {
+              "$push": { "user_ids": &user.user.id }
+            },
+        )
+        .await
+        .map_err(|err| error::ErrorInternalServerError(err))?;
+
+    Ok(web::Json(JoinResponse {}))
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.route("/", web::post().to(create));
+    cfg.route("/", web::post().to(create))
+        .route("/{id}", web::patch().to(join));
 }
